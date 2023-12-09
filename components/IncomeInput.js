1ac20f2d9815e5b192284b3e7f5f9d448ba6620e
ndri <5224431+ndri@users.noreply.github.com>
2023-12-09 23:30:18 +0100

    Improve input validation

--- components/IncomeInput.js
+++ components/IncomeInput.js
@@ -2,30 +2,24 @@ import { useState } from "react";
 import { equivalizeIncome } from "../utils/calculator";
 import { preventingDefault } from "../utils/utils";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import { usePlausible } from "next-plausible";
 
 export default function IncomeInput({ submitIncome }) {
-  let [income, setIncome] = useState("");
-  let [adults, setAdults] = useState(1);
-  let [children, setChildren] = useState(0);
-  let [changingHousehold, setChangingHousehold] = useState(false);
-
-  function onIncomeInputKeypress(event) {
-    const incomeInput = document.getElementById("incomeInput");
-
-    let newIncome = incomeInput.value.replace(",", ".");
-
-    // Must start with digit other than 0 and continue with any digit
-    // Can have a . with 0 to 2 digits at the end
-    // If it doesn't match, undo the input
-    if (/(^[1-9]\d{0,7}$)|(^[1-9]\d{0,7}\.\d{0,2}$)|^$/.test(newIncome)) {
-      setIncome(newIncome);
-    }
-  }
+  const [income, setIncome] = useState("");
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+  const [changingHousehold, setChangingHousehold] = useState(false);
+  const plausible = usePlausible();
 
   function submit() {
+    plausible("calculate");
     const equivalizedIncome = equivalizeIncome(income, adults, children);
     submitIncome(equivalizedIncome);
   }
+
+  const amountRegex = new RegExp(/^(\d+)?([.,]\d{1,2})?$/);
+  const valid =
+    amountRegex.test(income) && income > 0 && adults >= 1 && children >= 0;
 
   return (
     <form
@@ -45,7 +39,7 @@ export default function IncomeInput({ submitIncome }) {
             inputMode="numeric"
             className="block py-4 pr-10 pl-5 w-full rounded-md border-0 ring-1 ring-inset ring-slate-300 focus:ring-primary-600 text-slate-900 placeholder:text-slate-400 focus:ring-[3px] focus:ring-inset focus-within:outline-none"
             value={income}
-            onInput={onIncomeInputKeypress}
+            onInput={(event) => setIncome(event.target.value)}
           />
           <div className="flex absolute top-0 right-4 justify-center items-center h-full pointer-events-none select-none text-slate-400">
             €
@@ -114,6 +108,7 @@ export default function IncomeInput({ submitIncome }) {
         <button
           type="submit"
           className="block gap-1.5 justify-center items-center px-4 py-3 mt-4 w-full font-semibold tracking-tight text-center text-white rounded-md shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:opacity-50 disabled:cursor-not-allowed bg-primary-700 hover:bg-primary-600 focus-visible:outline-primary-700 disabled:hover:bg-primary-600"
+          disabled={!valid}
         >
           Arvuta!
         </button>
